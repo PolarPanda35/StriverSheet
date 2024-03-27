89abfb5fa5c2b8fac84e2c837be7f4b9d84352f1
Divyansh Devam <105588806+PolarPanda35@users.noreply.github.com>
2024-03-27 23:05:51 +0530

    Create 7.cpp

---
+++ Patterns/7.cpp
@@ -0,0 +1,16 @@
+void nStarTriangle(int n) {
+    // Write your code here.
+    int i,j;
+    for(i=0; i<n; i++){
+        for(j=0; j<n-i-1; j++){
+            cout << " ";
+        }
+        for(j=0; j<2*i+1; j++){
+            cout << "*";
+        }
+        for(j=0; j<n-i-1; j++){
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
