fba138480be544a3693821f753c3c3dfbe125852
Divyansh Devam <105588806+PolarPanda35@users.noreply.github.com>
2024-04-03 23:44:02 +0530

    Create ExplainList.cpp

---
+++ Learn the basic/Learn STL/ExplainList.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#include<list>
+using namespace std;
+
+int main(){
+    list<int> ls;
+    ls.push_back(1);//{1}
+    ls.emplace_back(2);//{1,2}
+
+    ls.push_front(3);//{3,1,2} -> push_front is more efficient in time complexity than insert of vectors
+    ls.emplace_front(4);//{4,3,1,2} 
+}
